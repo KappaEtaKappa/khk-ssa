c1972207fdca5b1babfeb656e924fdeb52da4b45
Joseph Dailey <josephldialey@gmail.com>
2017-09-19 08:08:13 +0000

    Update build script

--- build.sh
+++ build.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
 
 if ! which node > /dev/null 2>&1; then
-	echo "Node not installed. Install and retry"
+	echo !! Node not installed. Install and retry
 	exit 1
 fi
 if ! which npm > /dev/null 2>&1; then
-	echo "npm not installed. Install and retry"
+	echo !! npm not installed. Install and retry
 	exit 1
 fi
 
+echo :: Updatng Repository
 git pull origin master
+
+echo :: Installing Node Dependencies
 npm install
 
+echo :: Installing NGINX site files
+cd cp
 
+site=$(find . -maxdepth 1 -name "*.site")
+echo $site
+if [ -f $site ]; then
+ sudo cp $site /etc/nginx/sites/. -v
+else
+	echo !! Project is missing a NGINX ./cp/*.site config.
+  echo !!! Please see the README.
+  exit 1
+fi
+echo :: Testing updated NGINX site config for ${D#./}.
+echo ::: \(Previously loaded sites may have already broken NGINX\) 
+echo `sudo nginx -t`
 
+echo :: Installing System Service Daemon
+service=$(find . -maxdepth 1 -name "*.service")
+if [ -f $service ]; then
+	sudo cp $service /etc/systemd/system/.
+	sudo systemctl daemon-reload
+	sudo systemctl enable ${service#./}
+  sudo systemctl start ${service#./}
+  echo Press Q to Continue
+  sudo systemctl status ${service#./}
+else
+	echo !! Project is missing a systemd ./cp/*.service file.
+	echo !!! Please see the README.
+  exit 1
+fi
